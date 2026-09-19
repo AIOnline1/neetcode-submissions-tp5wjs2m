a7a7ed8b4135ed3bc9f0c819479b38788346c308
AIOnline1 <61701082+AIOnline1@users.noreply.github.com>
2026-09-19 14:03:07 +0100

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dic = defaultdict(list)
+
+        for string in strs:
+            alphabet = [0 for _ in range(26)]
+            for char in string:
+                alphabet[ord('a') - ord(char)] += 1
+            
+            dic[tuple(alphabet)].append(string)
+        
+        ans = []
+        for value in dic.values():
+            ans.append(value)
+        
+        return ans
